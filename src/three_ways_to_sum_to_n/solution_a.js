7475367fd3eba0da368b6f8903f9a508a57abd46
caipingkong <caiping5016@gmail.com>
2019-10-21 11:57:46 +0800

    Update solution_a.js

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -4,5 +4,10 @@
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
 var sum_to_n = function(n) {
-    // your code here
+   int n=0;
+    
+    for(int i=1;i<=5;i++){
+        n=n+i;
+    }
+    return n;
 };
